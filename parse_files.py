08e503a2deb2feccd35e74b335fb1b7f7030ff25
hafeezul_rahman <rahman.hafeezul@github.com>
2018-05-21 16:00:30 -0400

    parsing

---
+++ parse_files.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_excel(sys.argv[1], skiprows=[0])
+print(df.columns)
+df = df[['Area', 'Distance from Origin', 'Distance to Image Border XY',
+       'Ellipticity (oblate)', 'Ellipticity (prolate)', 'Intensity Max',
+       'Intensity Mean', 'Intensity Median', 'Intensity Min',
+       'Intensity StdDev', 'Intensity Sum', 'Number of Vertices', 'Position X',
+       'Position Y', 'Position Z', 'Sphericity', 'Volume']]
+print(df[:20])
